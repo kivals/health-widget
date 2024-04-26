94c7d93f4c03771e3c588fe2fdd19cf282ebb407
kivals <kivals.90@yandex.ru>
2024-04-26 23:54:05 +0300

    feat: Added Pulse page

--- src/pages/Pulse/Pulse.tsx
+++ src/pages/Pulse/Pulse.tsx
@@ -1,14 +1,60 @@
-import { FC } from "react";
+import { FC, useContext, useState } from "react";
+import { AppContext } from "../../context/AppContext.tsx";
+import { TAppContext } from "../../types/TAppContext.ts";
+import { IParameterConfig } from "../../types/IParameterConfig.ts";
+import { mockPulseData } from "../../mock/data.ts";
+import { TState } from "../../types/TState.ts";
 import PageLayout from "../PageLayout/PageLayout.tsx";
+import Parameter from "../../components/Parameter/Parameter.tsx";
+import { rangeValue } from "../../utils/common.ts";
 
 interface IPulse {
-  title: string,
+  title: string;
 }
 
 const Pulse: FC<IPulse> = ({ title }) => {
+  const { pulseHistory, setPulseHistory } = useContext(
+    AppContext,
+  ) as TAppContext;
+
+  const [pulseParameter, setPulseParameter] = useState<IParameterConfig>({
+    data: mockPulseData,
+    state: "show",
+  });
+
+  const onChangePulseState = (state: TState) => {
+    setPulseParameter((prev) => ({
+      ...prev,
+      state,
+    }));
+  };
+
+  const onChangePulseValue = (value: number) => {
+    setPulseParameter((prev) => ({
+      data: {
+        ...prev.data,
+        value,
+      },
+      state: "show",
+    }));
+
+    setPulseHistory([
+      ...pulseHistory,
+      rangeValue(
+        pulseParameter.data.minValue,
+        pulseParameter.data.maxValue,
+        value,
+      ),
+    ]);
+  };
   return (
     <PageLayout title={title}>
-      Pulse
+      <Parameter
+        data={pulseParameter.data}
+        state={pulseParameter.state}
+        onChangeParameter={onChangePulseValue}
+        onChangeState={onChangePulseState}
+      />
     </PageLayout>
   );
 };
