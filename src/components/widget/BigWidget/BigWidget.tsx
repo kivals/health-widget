afb35e413fc72eb5fe54b2307b7ba3ae3d27ee5b
kivals <kivals.90@yandex.ru>
2024-05-16 11:34:31 +0300

    fix: added stylized widget components

--- src/components/widget/BigWidget/BigWidget.tsx
+++ src/components/widget/BigWidget/BigWidget.tsx
@@ -1,30 +1,52 @@
 'use client';
 
 import clsx from 'clsx';
+import { useState } from 'react';
 
+import ApplyButton from '@/components/UI/buttons/ApplyButton/ApplyButton';
+import CancelButton from '@/components/UI/buttons/CancelButton/CancelButton';
 import ChangeButton from '@/components/UI/buttons/ChangeButton/ChangeButton';
+import NumberInput from '@/components/UI/inputs/NumberInput/NumberInput';
 import RangeInput from '@/components/UI/inputs/RangeInput/RangeInput';
 
 import styles from './BigWidget.module.scss';
 
 export function BigWidget() {
+	const [isEdit, setEdit] = useState<boolean>(false);
+
 	return (
 		<div className={clsx(styles.container)}>
-			<h2 className={clsx(styles.title)}>Систолическое давление</h2>
-			<span className={clsx(styles.value)}>120</span>
-			<span className={clsx(styles.units)}>мм. рт. ст.</span>
-			<div className={clsx(styles.range)}>
-				<RangeInput
-					maxValue={100}
-					minValue={0}
-					value={56}
-				/>
-			</div>
-			<ChangeButton
-				size={'lg'}
-				isDisabled={false}
-				onClick={() => console.log('')}
-			/>
+			<h2 className={clsx(styles.title)}>Систолическое давление2</h2>
+			{isEdit ? (
+				<>
+					<NumberInput
+						maxValue={0}
+						minValue={100}
+						onChange={() => console.log('')}
+					/>
+					<div className={styles.buttons}>
+						<ApplyButton onClick={() => console.log('')} />
+						<CancelButton onClick={() => setEdit(false)} />
+					</div>
+				</>
+			) : (
+				<>
+					<span className={clsx(styles.value)}>120</span>
+					<span className={clsx(styles.units)}>мм. рт. ст.</span>
+					<div className={clsx(styles.range)}>
+						<RangeInput
+							maxValue={100}
+							minValue={0}
+							value={56}
+						/>
+					</div>
+					<ChangeButton
+						size={'lg'}
+						isDisabled={false}
+						onClick={() => setEdit(true)}
+					/>
+				</>
+			)}
 		</div>
 	);
 }
--- src/components/widget/CompactWidget/CompactWidget.tsx
+++ src/components/widget/CompactWidget/CompactWidget.tsx
@@ -1,14 +1,55 @@
 import clsx from 'clsx';
+import { useState } from 'react';
+
+import ApplyButton from '@/components/UI/buttons/ApplyButton/ApplyButton';
+import CancelButton from '@/components/UI/buttons/CancelButton/CancelButton';
+import ChangeButton from '@/components/UI/buttons/ChangeButton/ChangeButton';
+import NumberInput from '@/components/UI/inputs/NumberInput/NumberInput';
+import RangeInput from '@/components/UI/inputs/RangeInput/RangeInput';
 
 import styles from './CompactWidget.module.scss';
 
 export function CompactWidget() {
+	const [isEdit, setEdit] = useState<boolean>(false);
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.header}>
-				<h2 className={clsx(styles.title)}>Систолическое давление</h2>
+				<h2 className={clsx(styles.title)}>Систолическое2 давление</h2>
+				{isEdit ? (
+					<div className={styles.buttons}>
+						<ApplyButton onClick={() => console.log('')} />
+						<CancelButton onClick={() => setEdit(false)} />
+					</div>
+				) : (
+					<ChangeButton
+						isDisabled={false}
+						onClick={() => setEdit(true)}
+						size={'sm'}
+					/>
+				)}
 			</div>
-			<div className={styles.body}></div>
+			<div className={styles.body}>
+				{isEdit ? (
+					<NumberInput
+						maxValue={0}
+						minValue={100}
+						onChange={() => console.log('')}
+					/>
+				) : (
+					<>
+						<span className={styles.value}>120</span>
+						<span className={styles.units}>мм. рт. ст.</span>
+						<div className={styles.range}>
+							<RangeInput
+								maxValue={100}
+								minValue={0}
+								value={56}
+							/>
+						</div>
+					</>
+				)}
+			</div>
 		</div>
 	);
 }
